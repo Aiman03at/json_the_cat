66326de6382ff0f2c04d28f50bb3602f8c8d73fc
Aiman03at <aiman03at@gmail.com>
2024-06-14 23:05:23 -0400

    Test the cat APi using Moch

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,34 +1,33 @@
 ////Users can provide any breed name, causing our application to fetch the information from the API and print out a short description of that breed.
 const needle=require("needle");
-////using readLine to take input
 
-var readline = require('readline');
-let breed;
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-const fetcher =() =>{
 
-rl.question("Enter breed name  ---", (input) => {
-  breed =input;
 
+const fetchBreedDescription = function(breed, callback) {
+  
   needle.get('https://api.thecatapi.com/v1/breeds', (error, response, body) => {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    const content=body;
-    for ( c of content) {
-      if (c.name === breed) {
-        console.log(c.description);
-        break;
-      }
-      
+    if (error) {
+      return callback(error, null);
     }
-    console.log ("Breed not found");
-});
+
+    if (response.statusCode !== 200) {
+      return callback(new Error(`Failed to fetch breed. Status code: ${response.statusCode}`), null);
+    }
+
+    const content = body;
+    
+    for (const c of content) {
+      if (c.name === breed) {
+        return callback(null, c.description);
+      }
+    }
+
+    callback("Breed not found", null);
+  });
+};
+
+  
+module.exports=fetchBreedDescription;
 
 
-});
 
-}
-fetcher();
